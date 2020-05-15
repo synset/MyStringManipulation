ba87b23c3a8c104fcdb221b4621bacd0db969c97
synset <synnove.settman@yahoo.com>
2020-05-15 15:55:29 +0200

    Add project files.

---
+++ MyStringManipulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyStringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a sentence: \n");
+            string sentence = Console.ReadLine();
+
+            string replace = sentence.Replace(" ", "_");
+            string leading = sentence.TrimStart(' ');
+            string trailing = sentence.TrimEnd(' ');
+            string upper = sentence.ToUpper();
+
+            Console.WriteLine("Original sentence: '" + sentence +
+                "'\nAfter replacing spaces with underscores: '" + replace +
+                "'\nAfter removing leading spaces: '" + leading +
+                "'\nAfter removing trailing spaces: '" + trailing +
+                "'\nAfter replacing lowercase with uppercase: '" + upper);
+        }
+    }
+}
